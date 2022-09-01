9ead9ee8a0ab023b8c236073b047dafcb9a0e3eb
Theo Puteaux <theo.ptx@laposte.net>
2022-09-01 09:31:24 +0200

    ajout du html opur les pages

--- src/pages/Employee.jsx
+++ src/pages/Employee.jsx
@@ -1,7 +1,11 @@
 export default function Employee() {
     return (
         <div>
-
+            <div id="employee-div" className="container">
+                <h1>Current Employees</h1>
+                <table id="employee-table" className="display"></table>
+                <a href="index.html">Home</a>
+            </div>
         </div>
     )
 }
