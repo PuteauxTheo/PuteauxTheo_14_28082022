8bbb157f92eea99a5adba6349b8a4129173b9af6
Theo Puteaux <theo.ptx@laposte.net>
2022-09-01 09:33:06 +0200

    ajout du store

--- src/index.jsx
+++ src/index.jsx
@@ -3,15 +3,19 @@ import ReactDOM from 'react-dom/client';
 import './style/css/style.css'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Provider } from "react-redux";
+import Home from './pages/Home.jsx'
+import Employee from "./pages/Employee";
+import store from "./utils/store"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-    <Provider >
+    <Provider store={store}>
         <React.StrictMode>
             <Router>
                 <Routes>
-                    <Route path="/"></Route>
+                    <Route path="/" element={<Home />}></Route>
+                    <Route path="employee-list" element={<Employee />}></Route>
                 </Routes>
             </Router>
         </React.StrictMode>
