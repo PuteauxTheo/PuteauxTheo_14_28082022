16fcc37c2f7f7df5e8afa7ba070a6abfad72700c
PuteauxTheo <91119961+PuteauxTheo@users.noreply.github.com>
2022-09-05 19:42:20 +0200

    add data information

---
+++ src/data/data.js
@@ -0,0 +1,37 @@
+export const employees = [
+    {
+        firstName: "Theo",
+        lastName: "Puteaux",
+        dateOfBirth: "06/18/2000",
+        startDate: "09/01/2022",
+        department: "Engineering",
+        street: "1 rue le maire",
+        city: "Nantes",
+        state: "Pays de la loire",
+        zipCode: "44000"
+    },
+    {
+        firstName: "Armand",
+        lastName: "Thierry",
+        dateOfBirth: "02/09/1998",
+        startDate: "10/21/2022",
+        department: "Engineering",
+        street: "2 rue le maire",
+        city: "Paris",
+        state: "Ile de France",
+        zipCode: "75000"
+    },
+    {
+        firstName: "Theo",
+        lastName: "Puteaux",
+        dateOfBirth: "11/18/1987",
+        startDate: "01/07/2022",
+        department: "Engineering",
+        street: "13 rue le maire",
+        city: "Le Mans",
+        state: "Pays de la loire",
+        zipCode: "72000"
+    }
+]
+
+localStorage.setItem("Employees",employees)
---
+++ src/data/departement.js
@@ -0,0 +1,1 @@
+export const departement = ["Sales", "Marketing", "Engineering", "Human Resources", "Legal"]
