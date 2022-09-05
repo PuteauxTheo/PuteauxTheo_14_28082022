901f843851bcc2bda195bc4b65ea7be061fa2616
PuteauxTheo <91119961+PuteauxTheo@users.noreply.github.com>
2022-09-05 19:46:34 +0200

    Add dropdown

--- src/components/DropDownMenu.jsx
+++ src/components/DropDownMenu.jsx
@@ -1,36 +1,23 @@
 import * as React from 'react';
 import Box from '@mui/material/Box';
-import InputLabel from '@mui/material/InputLabel';
-import MenuItem from '@mui/material/MenuItem';
-import FormControl from '@mui/material/FormControl';
-import Select from '@mui/material/Select';
+import Autocomplete from '@mui/material/Autocomplete';
+import TextField from '@mui/material/TextField';
 
-export default function DropDownMenu( { title } ) {
+export default function DropDownMenu({ title, data }) {
 
-  const [age, setAge] = React.useState('');
-
-  const handleChange = (event) => {
-    setAge(event.target.value);
-  };
   return (
     <div>
       <h3>{title}</h3>
       <Box sx={{ minWidth: 120 }}>
-      <FormControl fullWidth>
-        <InputLabel id="demo-simple-select-label">{title}</InputLabel>
-        <Select
-          labelId="demo-simple-select-label"
-          id="demo-simple-select"
-          value={age}
-          onChange={handleChange}
-        >
-          <MenuItem value={10}>Ten</MenuItem>
-          <MenuItem value={20}>Twenty</MenuItem>
-          <MenuItem value={30}>Thirty</MenuItem>
-        </Select>
-      </FormControl>
-    </Box>
+        <Autocomplete
+          disablePortal
+          id="combo-box-demo"
+          options={data}
+          sx={{ width: 300 }}
+          renderInput={(params) => <TextField {...params} label={title} />}
+        />
+      </Box>
     </div>
-    
+
   );
 }
