325caf9771c01c086233e390c90cbced6a5a9898
PuteauxTheo <91119961+PuteauxTheo@users.noreply.github.com>
2022-09-07 11:49:20 +0200

    prepare function for action

--- src/features/employee.js
+++ src/features/employee.js
@@ -2,22 +2,59 @@ import { createSlice } from "@reduxjs/toolkit";
 import { selectEmployee } from "../utils/selector";
 
 const initialState = {
-    statusEmployee : "void",
-    employee : "void",
-    error : null
+    dataEmployee : null,
+    isValidForm : false 
 }
 
 // action
 
+export function submitForm() {
+    return async (dispatch, getState) => {
+
+    }
+}
+
+export function isCorrect() {
+    return async (dispatch, getState) => {
+        
+    }
+}
 
 
+export function unvalideForm() {
+    return async (dispatch, getState) => {
+
+    }
+}
+
+export function validForm() {
+    return async (dispatch, getState) => {
+
+    }
+}
+
+export function resetForm() {
+    return async (dispatch, getState) => {
+
+    }
+}
 // reducer
 
 const { actions, reducer } = createSlice({
     name: 'form',
     initialState,
     reducer : {
-
+        submit: (draft, action) => {
+            draft.dataEmployee = action.payload            
+        },
+        unvalidForm: (draft, action) => {
+            draft.dataEmployee = action.payload
+            draft.isValidForm = false
+        },
+        validForm: (draft, action) => {
+            draft.dataEmployee = action.payload
+            draft.isValidForm = true
+        },
     }
 });
 
