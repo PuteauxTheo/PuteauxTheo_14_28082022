1ece99ad53463cc51478ad4301991748f2801555
PuteauxTheo <91119961+PuteauxTheo@users.noreply.github.com>
2022-09-07 16:56:02 +0200

    fix dropDown

--- src/components/DropDownMenu.jsx
+++ src/components/DropDownMenu.jsx
@@ -3,19 +3,30 @@ import Box from '@mui/material/Box';
 import Autocomplete from '@mui/material/Autocomplete';
 import TextField from '@mui/material/TextField';
 
-export default function DropDownMenu({ title, data }) {
+export default function DropDownMenu({ title, data, setValue, value}) {
+
 
   return (
     <div>
       <h3>{title}</h3>
       <Box sx={{ minWidth: 120 }}>
         <Autocomplete
-          disablePortal
-          id="combo-box-demo"
+          value={value}
+          onChange={(event, newValue) => {
+            console.log(" value de newValue "+newValue)
+            if(newValue === 'null'){
+              setValue()
+            }else{
+              setValue(newValue)
+            }
+            
+          }}
+          id="controllable-states-demo"
           options={data}
           sx={{ width: 300 }}
-          defaultValue={data[0]}
+          defaultValue=""
           renderInput={(params) => <TextField {...params} label={title} />}
+          
         />
       </Box>
     </div>
