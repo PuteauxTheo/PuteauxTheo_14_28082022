1418bb9558b98c83fa26210bc203e1a9da9991fd
PuteauxTheo <91119961+PuteauxTheo@users.noreply.github.com>
2022-09-08 12:31:36 +0200

    add function for action

--- src/features/employee.js
+++ src/features/employee.js
@@ -8,37 +8,25 @@ const initialState = {
 
 // action
 
-export function submitForm() {
-    return async (dispatch, getState) => {
-        const dataEmployee = selectEmployee(getState()).dataEmployee
-        dispatch(actions.submitForm(dataEmployee))
-    }
-}
 
-// export function isCorrect() {
+
+// export function isFormCorrect() {
 //     return async (dispatch, getState) => {
-
+//         const isValidForm = selectEmployee(getState()).isValidForm
+//         return isValidForm
 //     }
 // }
 
 
 export function unvalidForm() {
-    return async (dispatch, getState) => {
-        const isValidForm = selectEmployee(getState()).isValidForm
-
-        if(isValidForm){
-            dispatch(actions.unvalidForm())
-        }
+    return async (dispatch) => {
+        dispatch(actions.unvalidForm())
     }
 }
 
 export function validForm() {
-    return async (dispatch, getState) => {
-        const isValidForm = selectEmployee(getState()).isValidForm
-
-        if(!isValidForm){
-            dispatch(actions.validForm())
-        }
+    return async (dispatch) => {
+        dispatch(actions.validForm())
     }
 }
 
@@ -47,22 +35,39 @@ export function resetForm() {
         dispatch(actions.reset())
     }
 }
+
+export function submitForm(newEmployee) {
+    return async (dispatch, getState) => {
+        const isFormCorrect = selectEmployee(getState()).isValidForm
+        console.log("isformCorrect ? "+isFormCorrect)
+        if(isFormCorrect){
+            dispatch(actions.submit(newEmployee))
+            return true
+        }else{
+            return false
+        }
+        
+    }
+}
 // reducer
 
 const { actions, reducer } = createSlice({
     name: 'form',
     initialState,
-    reducer : {
+    reducers : {
         submit: (draft, action) => {
-            draft.dataEmployee = action.payload            
+            draft.dataEmployee = action.payload   
+            return         
         },
         unvalidForm: (draft, action) => {
             draft.dataEmployee = action.payload
             draft.isValidForm = false
+            return
         },
         validForm: (draft, action) => {
             draft.dataEmployee = action.payload
             draft.isValidForm = true
+            return
         },
         reset: () => {
             return initialState;
